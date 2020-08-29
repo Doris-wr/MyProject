ff6ddc2fd46a65ac57c83de489613a74c948de31
Doris-wr <2239456274@qq.com>
2020-08-29 23:41:56 +0800

    复习

---
+++ 进制相关/HexTest.java
@@ -0,0 +1,10 @@
+package 进制相关;
+
+import java.lang.String;
+
+public class HexTest {
+    public static void main(String[] args) {
+        int adc = Integer.parseInt("ADC", 16);
+        System.out.println(adc);
+    }
+}
